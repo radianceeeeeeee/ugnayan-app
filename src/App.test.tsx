22bf9f523d8a47e2bfc7ab3632daddcfbbc7da77
radianceeeeeeee <97012089+radianceeeeeeee@users.noreply.github.com>
2024-02-29 21:26:00 +0800

    Set up empty test case suite

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,11 @@
+// Test case suite setup credit: https://codingpr.com/test-your-react-app-with-vitest-and-react-testing-library/
+
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Click on the Vite and React logos to learn more/i);
   expect(linkElement).toBeInTheDocument();
 });

554966fa287b071fb4561736921a41e21b2e3eec
Louis-Miguel <lpfermiza@up.edu.ph>
2024-05-01 18:55:20 +0800

    added the orgDescription text

--- src/pages/AdminPage/AdminModalAdd.tsx
+++ src/pages/AdminPage/AdminModalAdd.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Button, FloatingLabel, Form } from "react-bootstrap";
 import { doTestCreateOrgWithEmailAndPassword } from "../../firebase/auth";
+import "./AdminModalAdd.css"
 
 const AdminModalAdd = () => {
   const [error, setError] = useState({
@@ -62,23 +63,22 @@ const AdminModalAdd = () => {
   });
 
   async function handleSubmit(e: any) {
-      e.preventDefault();
-      const form = e.target;
+    e.preventDefault();
+    const form = e.target;
 
-      if (!form.checkValidity()) {
-        form.classList.add("was-validated");
-        return;
-      }
-      await doTestCreateOrgWithEmailAndPassword(formData);
-      alert("Organization is now authenticated")
+    if (!form.checkValidity()) {
+      form.classList.add("was-validated");
+      return;
+    }
+    await doTestCreateOrgWithEmailAndPassword(formData);
+    alert("Organization is now authenticated");
   }
 
   const handleChange = (event: any) => {
     const value = event.target.value;
     const id = event.target.id;
 
-    console.log(`ID: ${id}; Value: ${value}`)
-
+    console.log(`ID: ${id}; Value: ${value}`);
 
     setFormData({
       ...formData,
@@ -86,144 +86,214 @@ const AdminModalAdd = () => {
     });
   };
 
-
   return (
     <>
       <div className="modal-body">
         <Form onSubmit={handleSubmit} id="org-form">
-          <FloatingLabel
-            controlId="orgConnectedEmail"
-            label="Email address"
-            className="mb-3"
-          >
-            <Form.Control type="email" placeholder="name@example.com" onChange={handleChange}/>
-          </FloatingLabel>
+          <div className="add-modal-body">
+            <div>
+              <FloatingLabel
+                controlId="orgConnectedEmail"
+                label="Email address"
+                className="mb-3"
+              >
+                <Form.Control
+                  type="email"
+                  placeholder="name@example.com"
+                  onChange={handleChange}
+                />
+              </FloatingLabel>
 
-          <FloatingLabel
-            controlId="orgPassword"
-            label="Password"
-            className="mb-3"
-          >
-            <Form.Control type="password" placeholder="Password" onChange={handleChange}/>
-          </FloatingLabel>
+              <FloatingLabel
+                controlId="orgPassword"
+                label="Password"
+                className="mb-3"
+              >
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  onChange={handleChange}
+                />
+              </FloatingLabel>
 
-          <FloatingLabel
-            controlId="orgName"
-            label="Organization Name"
-            className="mb-3"
-          >
-            <Form.Control placeholder="Organization Name" onChange={handleChange}/>
-          </FloatingLabel>
+              <FloatingLabel
+                controlId="orgName"
+                label="Organization Name"
+                className="mb-3"
+              >
+                <Form.Control
+                  placeholder="Organization Name"
+                  onChange={handleChange}
+                />
+              </FloatingLabel>
 
-          <FloatingLabel
-            controlId="orgLogo"
-            label="Organization Logo"
-            className="mb-3"
-          >
-            <Form.Control placeholder="Organization Logo (imgur link)" onChange={handleChange}/>
-          </FloatingLabel>
+              <FloatingLabel
+                controlId="orgLogo"
+                label="Organization Logo"
+                className="mb-3"
+              >
+                <Form.Control
+                  placeholder="Organization Logo (imgur link)"
+                  onChange={handleChange}
+                />
+              </FloatingLabel>
 
-          <FloatingLabel
-            controlId="orgAcronym"
-            label="Organization Acronym"
-            className="mb-3"
-          >
-            <Form.Control placeholder="Organization Acronym" onChange={handleChange}/>
-          </FloatingLabel>
+              <FloatingLabel
+                controlId="orgAcronym"
+                label="Organization Acronym"
+                className="mb-3"
+              >
+                <Form.Control
+                  placeholder="Organization Acronym"
+                  onChange={handleChange}
+                />
+              </FloatingLabel>
 
-          <FloatingLabel
-            controlId="orgPictures"
-            label="Organization Picture"
-            className="mb-3"
-          >
-            <Form.Control placeholder="Organization Picture (imgur link, separate by comma)" onChange={handleChange}/>
-          </FloatingLabel>
+              <FloatingLabel
+                controlId="orgPictures"
+                label="Organization Picture"
+                className="mb-3"
+              >
+                <Form.Control
+                  placeholder="Organization Picture (imgur link, separate by comma)"
+                  onChange={handleChange}
+                />
+              </FloatingLabel>
 
-          <FloatingLabel
-            controlId="orgBio"
-            label="Organization Bio"
-            className="mb-3"
-          >
-            <Form.Control placeholder="Insert bio here" onChange={handleChange}/>
-          </FloatingLabel>
-          
+              <FloatingLabel
+                controlId="orgBio"
+                label="Organization Bio"
+                className="mb-3"
+              >
+                <Form.Control
+                  placeholder="Insert bio here"
+                  onChange={handleChange}
+                />
+              </FloatingLabel>
 
-          <FloatingLabel
-            controlId="orgTags"
-            label="Organization Tags"
-            className="mb-3"
-          >
-            <Form.Control placeholder="Insert tags here separated by comma" onChange={handleChange}/>
-          </FloatingLabel>
+              <FloatingLabel
+                controlId="orgTags"
+                label="Organization Tags"
+                className="mb-3"
+              >
+                <Form.Control
+                  placeholder="Insert tags here separated by comma"
+                  onChange={handleChange}
+                />
+              </FloatingLabel>
 
-          <FloatingLabel
-            controlId="dateFounded"
-            label="Organization Date Founded"
-            className="mb-3"
-          >
-            <Form.Control placeholder="Organization Date Founded" onChange={handleChange}/>
-          </FloatingLabel>
+              <FloatingLabel
+                controlId="dateFounded"
+                label="Organization Date Founded"
+                className="mb-3"
+              >
+                <Form.Control
+                  placeholder="Organization Date Founded"
+                  onChange={handleChange}
+                />
+              </FloatingLabel>
 
-          <FloatingLabel
-            controlId="orgLocation"
-            label="Organization Location"
-            className="mb-3"
-          >
-            <Form.Control placeholder="Organization Location" onChange={handleChange}/>
-          </FloatingLabel>
+              <FloatingLabel
+                controlId="orgLocation"
+                label="Organization Location"
+                className="mb-3"
+              >
+                <Form.Control
+                  placeholder="Organization Location"
+                  onChange={handleChange}
+                />
+              </FloatingLabel>
 
-          <FloatingLabel
-            controlId="orgAffiliations"
-            label="Organization Affiliations"
-            className="mb-3"
-          >
-            <Form.Control placeholder="Insert org affiliations, separated by comma" onChange={handleChange}/>
-          </FloatingLabel>
+              <FloatingLabel
+                controlId="orgAffiliations"
+                label="Organization Affiliations"
+                className="mb-3"
+              >
+                <Form.Control
+                  placeholder="Insert org affiliations, separated by comma"
+                  onChange={handleChange}
+                />
+              </FloatingLabel>
 
-          <FloatingLabel
-            controlId="orgEmails"
-            label="Organization Emails"
-            className="mb-3"
-          >
-            <Form.Control placeholder="Insert org emails, separated by comma" onChange={handleChange}/>
-          </FloatingLabel>
+              <FloatingLabel
+                controlId="orgEmails"
+                label="Organization Emails"
+                className="mb-3"
+              >
+                <Form.Control
+                  placeholder="Insert org emails, separated by comma"
+                  onChange={handleChange}
+                />
+              </FloatingLabel>
 
-          <FloatingLabel
-            controlId="orgFacebook"
-            label="Organization Facebook"
-            className="mb-3"
-          >
-            <Form.Control placeholder="Insert org facebook link" onChange={handleChange}/>
-          </FloatingLabel>
+              <FloatingLabel
+                controlId="orgFacebook"
+                label="Organization Facebook"
+                className="mb-3"
+              >
+                <Form.Control
+                  placeholder="Insert org facebook link"
+                  onChange={handleChange}
+                />
+              </FloatingLabel>
 
-          <FloatingLabel
-            controlId="orgWebsite"
-            label="Organization Website"
-            className="mb-3"
-          >
-            <Form.Control placeholder="Insert org website" onChange={handleChange}/>
-          </FloatingLabel>
+              <FloatingLabel
+                controlId="orgWebsite"
+                label="Organization Website"
+                className="mb-3"
+              >
+                <Form.Control
+                  placeholder="Insert org website"
+                  onChange={handleChange}
+                />
+              </FloatingLabel>
 
-          <FloatingLabel
-            controlId="orgScope"
-            label="Organization Scope"
-            className="mb-3"
-          >
-            <Form.Control placeholder="Insert org scope" onChange={handleChange}/>
-          </FloatingLabel>
+              <FloatingLabel
+                controlId="orgScope"
+                label="Organization Scope"
+                className="mb-3"
+              >
+                <Form.Control
+                  placeholder="Insert org scope"
+                  onChange={handleChange}
+                />
+              </FloatingLabel>
 
-          <FloatingLabel
-            controlId="orgDescription"
-            label="Organization Description"
-            className="mb-3"
-          >
-            <Form.Control placeholder="Insert org description" as="textarea" onChange={handleChange}/>
-          </FloatingLabel>
+              <FloatingLabel
+                controlId="orgDescription"
+                label="Organization Description"
+                className="mb-3"
+              >
+                <Form.Control
+                  placeholder="Insert org description"
+                  as="textarea"
+                  onChange={handleChange}
+                />
+              </FloatingLabel>
+            </div>
+            <div>
+              <FloatingLabel
+                controlId="orgDescription"
+                className="mb-3 add-modal-description"
+                label="Description"
+              >
+                <Form.Control
+                  placeholder="Description"
+                  onChange={handleChange}
+                  style={{ height: "50vh" }}
+                />
+              </FloatingLabel>
+            </div>
+          </div>
         </Form>
       </div>
       <div className="modal-footer">
-        <Button className="btn btn-secondary" data-bs-dismiss="modal">Cancel</Button>
-        <Button form="org-form" className="btn btn-primary" type="submit">Submit</Button>
+        <Button className="btn btn-secondary" data-bs-dismiss="modal">
+          Cancel
+        </Button>
+        <Button form="org-form" className="btn btn-primary" type="submit">
+          Submit
+        </Button>
       </div>
     </>
   );

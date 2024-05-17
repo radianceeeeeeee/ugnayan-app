7e01317ecda29f07f29e4b139b6532ba6e7bd7e9
radianceeeeeeee <97012089+radianceeeeeeee@users.noreply.github.com>
2024-05-17 08:14:34 +0800

    Added test for rendering application status

---
+++ tests/OrgPage.test.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import OrgPage from '../src/pages/OrgPage/OrgPage.tsx'
+import { MemoryRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+
+// testing routed pages credit from: https://stackoverflow.com/questions/76081552/typeerror-cannot-destructure-property-basename-of-react-namespace-usecontex
+
+test('org page must display "Sign up to apply to this organization" by default', () => {
+    render(
+      <MemoryRouter><OrgPage /></MemoryRouter>);
+    const notice = screen.getByText(/Sign up to apply to this organization/i);
+  
+    expect(notice).toBeInTheDocument();
+  });
